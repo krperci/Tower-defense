36f4a9e8f0211d74f76e219457e8aa4fdc268694
FedorSennik <fedorsennik@gmail.com>
2025-03-02 20:34:07 +0200

    +Enemy Data

---
+++ Assets/Scripts/TowerData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "NewTowerData", menuName = "NewTower")]
+public class TowerData : ScriptableObject
+{
+    public GameObject bulletPrefab;
+    public float range;
+    public float damage;
+}
