a8db225c59527b2e8daf9586260728f491e01541
FedorSennik <fedorsennik@gmail.com>
2025-03-16 19:17:14 +0200

    +Покупка за гроші башеі

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -1,33 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
     Player player;
+
+    private int ScoutPrice = 75;
+    private int SoldierPrice = 125;
+    private int SniperPrice = 150;
+
+    [SerializeField] TextMeshProUGUI scoutPrice;
+    [SerializeField] TextMeshProUGUI soldierPrice;
+    [SerializeField] TextMeshProUGUI sniperPrice;
+
+    private void Start()
+    {
+        scoutPrice.text = ScoutPrice.ToString();
+        soldierPrice.text = SoldierPrice.ToString();
+        sniperPrice.text = SniperPrice.ToString();
+    }
+
     public void PurchaseScout()
     {
-        if (player.coins >= 75)
+        if (player.coins >= ScoutPrice)
         {
             BuildManager.instance.SetTowerToBuild(BuildManager.instance.Scout);
-            player.coins =- 75;
+            player.coins =- ScoutPrice;
         }
     }
 
     public void PurchaseSoldier()
     {
-        if (player.coins >= 125)
+        if (player.coins >= SoldierPrice)
         {
             BuildManager.instance.SetTowerToBuild(BuildManager.instance.Soldier);
-            player.coins = -125;
+            player.coins =- SoldierPrice;
         }
     }
     public void PurchaseSniper()
     {
-        if (player.coins >= 150)
+        if (player.coins >= SniperPrice)
         {
             BuildManager.instance.SetTowerToBuild(BuildManager.instance.Sniper);
-            player.coins = -150;
+            player.coins =- SniperPrice;
         }
     }
 }
